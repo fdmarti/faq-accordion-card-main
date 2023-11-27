c0467ff54ae81644614afa4aeb2e4bef3117401c
f-d.martinez <f-d.martinez@viseven.com>
2023-11-26 23:13:43 -0300

    first commit

---
+++ src/components/ImageComponents/index.js
@@ -0,0 +1,3 @@
+export { default as IllustrationBgPattern } from './IllustrationBgPattern.astro'
+export { default as IllustrationBoxComponent } from './IllustrationBoxComponent.astro'
+export { default as IllustrationWomanComponent } from './IllustrationWomanComponent.astro'
